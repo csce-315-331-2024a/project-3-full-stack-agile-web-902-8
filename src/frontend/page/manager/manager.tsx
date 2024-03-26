6fef88a9d61a8fd17f01b81e53d259c1dcf7db3f
Jose-Ortiz47 <josepascualos17@tamu.edu>
2024-03-26 13:32:02 -0500

    Folder layout for frontend

---
+++ src/frontend/page/manager/manager.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Manager() {
+  return (
+    <div>
+      <h1>Manager Page</h1>
+      <p>This is the manager page content.</p>
+    </div>
+  );
+}
--- src/frontend/page/menuboards/menuboards.tsx
+++ src/frontend/page/menuboards/menuboards.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Link from "next/link"; // Import Link from Next.js
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Hello World&nbsp;
+          Menu Boards&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
       </div>
