3ffc907684da15f050afcd569510f79862a6f202
blubywaff <45488563+blubywaff@users.noreply.github.com>
2024-03-26 13:40:48 -0500

    reformat orders.tsx

--- src/lib/orders.ts
+++ src/lib/orders.ts
@@ -10,14 +10,14 @@ import postgres from "postgres";
 export async function addOrder(o: Order, tsql = psql): Promise<number> {
     const p = new Promise((R: (v: number) => void, F: (e: Error<postgres.Error, Order>) => void) => {
         beginOrContinue<number>(tsql, async isql => {
-            let [{id: id}] = await isql`INSERT INTO orders (timestamp, discount, total) VALUES (${o.timestamp}, ${o.discount}, ${o.total}) RETURNING id;`
+            let [{ id: id }] = await isql`INSERT INTO orders (timestamp, discount, total) VALUES (${o.timestamp}, ${o.discount}, ${o.total}) RETURNING id;`
             for (const item of o.items) {
                 await isql`INSERT INTO order_items (order_id, item_id, qty) VALUES (${id}, ${item.item.id}, ${item.quantity});`
             }
             return id;
         })
-        .then((id) => {R(id);})
-        .catch((e) => {F(new Error("SQL Error in addOrder", e, o));});
+            .then((id) => { R(id); })
+            .catch((e) => { F(new Error("SQL Error in addOrder", e, o)); });
     });
     return p;
 }
@@ -28,11 +28,11 @@ export async function addOrder(o: Order, tsql = psql): Promise<number> {
  * @param id The id of the inventory item to check.
  * @return true if and only if the stock of that item is greater than amount.
  */
-export async function enoughInventory(amount: number, id: number, tsql = psql ): Promise<boolean> {
+export async function enoughInventory(amount: number, id: number, tsql = psql): Promise<boolean> {
     const p = new Promise((R: (v: boolean) => void, F: (e: Error<postgres.Error>) => void) => {
         tsql`SELECT qty > ${amount} AS enough FROM inventory WHERE id = ${id}`
-        .then(([{enough: b}]) => R(b))
-        .catch((e) => {F(new Error("SQL Error in enoughInventory", e, {amount: amount, id: id}));});
+            .then(([{ enough: b }]) => R(b))
+            .catch((e) => { F(new Error("SQL Error in enoughInventory", e, { amount: amount, id: id })); });
     });
     return p;
 }
