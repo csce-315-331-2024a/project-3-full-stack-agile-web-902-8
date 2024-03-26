2a793552adea6f0b35e9c1298a7456c34e97d766
blubywaff <45488563+blubywaff@users.noreply.github.com>
2024-03-26 11:35:41 -0500

    orders use new database system (type annotations, beginOrContinue)

--- src/lib/orders.ts
+++ src/lib/orders.ts
@@ -1,21 +1,23 @@
-import psql from "@/lib/database"
+import psql, { beginOrContinue } from "@/lib/database"
 import { Order } from "@/lib/models";
 import Error from "@/lib/error";
+import postgres from "postgres";
 
 /**
  * @param o The order to submit to the database.
  * @return The id of the added order.
  */
-export async function addOrder(o: Order): Promise<number> {
-    const p = new Promise((R: (v: number) => void, F: (e: Error) => void) => {
-        psql.begin(async psql => {
-            let [{id: id}] = await psql`INSERT INTO orders (timestamp, discount, total) VALUES (${o.timestamp}, ${o.discount}, ${o.total}) RETURNING id;`
+export async function addOrder(o: Order, tsql = psql): Promise<number> {
+    const p = new Promise((R: (v: number) => void, F: (e: Error<postgres.Error, Order>) => void) => {
+        beginOrContinue<number>(tsql, async isql => {
+            let [{id: id}] = await isql`INSERT INTO orders (timestamp, discount, total) VALUES (${o.timestamp}, ${o.discount}, ${o.total}) RETURNING id;`
             for (const item of o.items) {
-                await psql`INSERT INTO order_items (order_id, item_id, qty) VALUES (${id}, ${item.item.id}, ${item.quantity});`
+                await isql`INSERT INTO order_items (order_id, item_id, qty) VALUES (${id}, ${item.item.id}, ${item.quantity});`
             }
             return id;
-        }).then((id) => {R(id);})
-        .catch((e) => {F(new Error("SQL Error in addOrder", e));});
+        })
+        .then((id) => {R(id);})
+        .catch((e) => {F(new Error("SQL Error in addOrder", e, o));});
     });
     return p;
 }
@@ -26,11 +28,11 @@ export async function addOrder(o: Order): Promise<number> {
  * @param id The id of the inventory item to check.
  * @return true if and only if the stock of that item is greater than amount.
  */
-export async function enoughInventory(amount: number, id: number, _psql = psql ): Promise<boolean> {
-    const p = new Promise((R: (v: boolean) => void, F: (e: Error) => void) => {
-        _psql`SELECT qty > ${amount} AS enough FROM inventory WHERE id = ${id}`
+export async function enoughInventory(amount: number, id: number, tsql = psql ): Promise<boolean> {
+    const p = new Promise((R: (v: boolean) => void, F: (e: Error<postgres.Error>) => void) => {
+        tsql`SELECT qty > ${amount} AS enough FROM inventory WHERE id = ${id}`
         .then(([{enough: b}]) => R(b))
-        .catch((e) => {F(new Error("SQL Error in enoughInventory", e));});
+        .catch((e) => {F(new Error("SQL Error in enoughInventory", e, {amount: amount, id: id}));});
     });
     return p;
 }
