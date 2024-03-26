75ea185475b3726f4274ea0859e3c081864f3dd4
blubywaff <45488563+blubywaff@users.noreply.github.com>
2024-03-26 18:45:24 -0500

    change orders.ts to use database transact instead of old beginOrContinue

--- src/lib/orders.ts
+++ src/lib/orders.ts
@@ -1,4 +1,4 @@
-import psql, { beginOrContinue } from "@/lib/database"
+import psql, { transact } from "@/lib/database"
 import { Order } from "@/lib/models";
 import Error from "@/lib/error";
 import postgres from "postgres";
@@ -8,18 +8,13 @@ import postgres from "postgres";
  * @return The id of the added order.
  */
 export async function addOrder(o: Order, tsql = psql): Promise<number> {
-    const p = new Promise((R: (v: number) => void, F: (e: Error<postgres.Error, Order>) => void) => {
-        beginOrContinue<number>(tsql, async isql => {
-            let [{ id: id }] = await isql`INSERT INTO orders (timestamp, discount, total) VALUES (${o.timestamp}, ${o.discount}, ${o.total}) RETURNING id;`
-            for (const item of o.items) {
-                await isql`INSERT INTO order_items (order_id, item_id, qty) VALUES (${id}, ${item.item.id}, ${item.quantity});`
-            }
-            return id;
-        })
-            .then((id) => { R(id); })
-            .catch((e) => { F(new Error("SQL Error in addOrder", e, o)); });
+    return transact<number, postgres.Error, any>(tsql, new Error("SQL Error in addOrder", undefined, o), async (isql, _) => {
+        let [{ id: id }] = await isql`INSERT INTO orders (timestamp, discount, total) VALUES (${o.timestamp}, ${o.discount}, ${o.total}) RETURNING id;`
+        for (const item of o.items) {
+            await isql`INSERT INTO order_items (order_id, item_id, qty) VALUES (${id}, ${item.item.id}, ${item.quantity});`
+        }
+        return id;
     });
-    return p;
 }
 
 /**
@@ -29,10 +24,8 @@ export async function addOrder(o: Order, tsql = psql): Promise<number> {
  * @return true if and only if the stock of that item is greater than amount.
  */
 export async function enoughInventory(amount: number, id: number, tsql = psql): Promise<boolean> {
-    const p = new Promise((R: (v: boolean) => void, F: (e: Error<postgres.Error>) => void) => {
-        tsql`SELECT qty > ${amount} AS enough FROM inventory WHERE id = ${id}`
-            .then(([{ enough: b }]) => R(b))
-            .catch((e) => { F(new Error("SQL Error in enoughInventory", e, { amount: amount, id: id })); });
+    return transact<boolean, postgres.Error, any>(tsql, new Error("SQL Error in enoughInventory", undefined, { amount: amount, id: id }), async (isql, _) => {
+        let [{ enough: b }] = await isql`SELECT qty > ${amount} AS enough FROM inventory WHERE id = ${id}`;
+        return b;
     });
-    return p;
 }
