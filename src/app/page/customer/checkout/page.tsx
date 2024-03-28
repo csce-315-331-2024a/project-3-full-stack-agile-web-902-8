b7c5ad31dc283de9ef26c733a8339dbef6dd3798
Marvin Fung <marfung37@tamu.edu>
2024-03-27 23:42:14 -0500

    Added basic structure for customer page

--- src/app/page/customer/checkout/page.tsx
+++ src/app/page/customer/checkout/page.tsx
@@ -1,18 +1,24 @@
+import styles from "@/app/page.module.css";
+import customerStyles from "@/app/page/customer/page.module.css";
 import Link from "next/link";
 
 export default function CustomerCheckout() {
     return (
-      <main>
-        <nav>
-          <Link href="/page/customer">
+      <main className={styles.main}>
+        <h1>Customer</h1>
+
+        <nav id={customerStyles["customer-navbar"]}>
+          <ul>
+          <li><Link href="/page/customer">
             home
-          </Link>
-          <Link href="/page/customer/order">
+          </Link></li>
+          <li><Link href="/page/customer/order">
             order
-          </Link>
-          <Link href="/page/customer/checkout">
+          </Link></li>
+          <li><Link href="/page/customer/checkout">
             checkout
-          </Link>
+          </Link></li>
+          </ul>
         </nav>
       </main>
     );
