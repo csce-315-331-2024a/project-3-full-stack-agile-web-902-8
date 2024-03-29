1622beb5187d329eda3a2ef5431d1288588af274
tran-nathan36 <tran.nathan36@tamu.edu>
2024-03-29 17:47:26 -0500

    Added component for manager order table, adjusted css files, manager page template is now completely finished barring small adjustments

--- src/app/manager/page.tsx
+++ src/app/manager/page.tsx
@@ -3,6 +3,7 @@ import Heading from "@/components/Heading";
 import PageButton from "@/components/PageButton";
 import DoubleText from "@/components/DoubleText";
 import SideBar from "@/components/SideBar";
+import OrderTable from "@/components/OrderTable";
 import styles from '../page.module.css';
 
 export default function Manager() {
@@ -10,6 +11,9 @@ export default function Manager() {
   const Links = ['/manager', '/manager', '/manager', '/manager', '/manager', '/'];
   const Items2 = ['Manager', 'Customer', 'Cashier', 'MenuBoard'];
   const Links2 = ['/manager', '/customer', '/cashier', '/menuboards'];
+  const tableHead = ['TimeStamp', 'Order_Id', 'Discount', 'Total'];
+  const tableBody = [['Sample time 1', 'Sample id 1', 'Sample Discount 1', 'Sample Total 1'],
+                    ['Sample time 2', 'Sample id 2', 'Sample Discount 2', 'Sample Total 2']]; 
   
   return (
     <main className={styles.main}>
@@ -25,9 +29,10 @@ export default function Manager() {
             <h1>Manager Page</h1>
 
             <PageButton>Refresh</PageButton>
-            <p>
+            {/*<p>
               TimeStamp | Order_Id | Discount | Total
-            </p>
+  </p>*/}
+                  <OrderTable heading = {tableHead} rows = {tableBody}/>
         </div>/>
         </div>
       </div>
