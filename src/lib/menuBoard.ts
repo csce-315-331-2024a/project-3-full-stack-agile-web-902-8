50b45a1328925b615d4dd3376e349e1f89523459
Sandeep <sandeep2002mishra@tamu.edu>
2024-03-28 20:40:12 -0500

    Fixed issues with aggregateMenutItems function

--- src/lib/menuBoard.ts
+++ src/lib/menuBoard.ts
@@ -1,31 +1,29 @@
 import psql, { transact } from "@/lib/database"
-import { MenuItem } from "@/lib/models";
 import { Seasonal } from "@/lib/models";
+import { AggregateItem } from "@/lib/models";
 import Error from "@/lib/error";
 import postgres from "postgres";
 
 export async function aggregateMenuItems(startDate: Date, endDate: Date, tsql = psql): Promise<AggregateItem[]>{
-    return transact<AggregateItem[], postgres.Error, any>(tsql, new Error("SQL Error in removeFromInventory", undefined, {startDate, endDate}), async (isql, abort) => {
-        const query = `
-        SELECT
-          mi.id,
-          mi.name,
-          SUM(oi.quantity) AS qty
-        FROM
-          menu_items mi
-        JOIN
-          order_items oi ON mi.id = oi.menu_item_id
-        JOIN
-          orders o ON oi.order_id = o.id
-        WHERE
-          o.timestamp >= ${startDate.toISOString()} AND
-          o.timestamp <= ${endDate.toISOString()}
-        GROUP BY
-          mi.id;
-      `;
-      
+    return transact<AggregateItem[], postgres.Error, any>(tsql, new Error("SQL Error in removeFromInventory", undefined, {startDate, endDate}), async (isql, abort) => { 
       const items: AggregateItem[] = [];
-      const result = await isql.any(query);
+      const result = await isql`
+      SELECT
+        mi.id,
+        mi.name,
+        SUM(oi.quantity) AS qty
+      FROM
+        menu_items mi
+      JOIN
+        order_items oi ON mi.id = oi.menu_item_id
+      JOIN
+        orders o ON oi.order_id = o.id
+      WHERE
+        o.timestamp >= ${startDate.toISOString()} AND
+        o.timestamp <= ${endDate.toISOString()}
+      GROUP BY
+        mi.id;
+    `;
       
       for (const row of result) {
         items.push(new AggregateItem(row.id, row.name, row.qty));
