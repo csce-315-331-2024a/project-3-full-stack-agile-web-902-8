23a858915249e906f544575222d5384bf70c5341
Sandeep <sandeep2002mishra@tamu.edu>
2024-03-28 21:13:17 -0500

    Added page for menuBoard

--- src/app/menuboards/page.tsx
+++ src/app/menuboards/page.tsx
@@ -1,11 +1,37 @@
+import React from "react";
+import Heading from "@/components/Heading";
+import PageButton from "@/components/PageButton";
+import DoubleText from "@/components/DoubleText";
+import SideBar from "@/components/SideBar";
+import styles from '../page.module.css';
+
 export default function MenuBoard() {
+  const Items = ['Home', 'Menu', 'Inventory', 'Order History', 'Reports', 'Logout'];
+  const Links = ['/', '/', '/', '/', '/', '/'];
+  const Items2 = ['Manager', 'Customer', 'Cashier', 'MenuBoard'];
+  const Links2 = ['/', '/', '/', '/'];
+  
   return (
-    <main>
-      <div>
-        <p>
-          Menu Boards
-        </p>
+    <main className={styles.main}>
+      <div className = {styles.description}>
+        <div>
+          <Heading names={Items} hrefs={Links}/>
+        </div>
+        <div className={styles.body}>
+          <DoubleText 
+            block1 = <SideBar names={Items2} hrefs={Links2}/>
+            block2 = 
+            <div>
+            <h1>MenuBoard Page</h1>
+
+            <PageButton>Refresh</PageButton>
+            <p>
+              TimeStamp | Order_Id | Discount | Total
+            </p>
+        </div>/>
+        </div>
       </div>
     </main>
   );
 }
+
