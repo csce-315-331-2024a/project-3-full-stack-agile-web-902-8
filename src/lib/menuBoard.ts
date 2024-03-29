bc607257e0a435cd69e3d3755cf46f3b4bcc8e21
Sandeep <sandeep2002mishra@tamu.edu>
2024-03-28 20:35:40 -0500

    Added the AggregateItem class to use

--- src/lib/menuBoard.ts
+++ src/lib/menuBoard.ts
@@ -6,7 +6,32 @@ import postgres from "postgres";
 
 export async function aggregateMenuItems(startDate: Date, endDate: Date, tsql = psql): Promise<AggregateItem[]>{
     return transact<AggregateItem[], postgres.Error, any>(tsql, new Error("SQL Error in removeFromInventory", undefined, {startDate, endDate}), async (isql, abort) => {
-        
+        const query = `
+        SELECT
+          mi.id,
+          mi.name,
+          SUM(oi.quantity) AS qty
+        FROM
+          menu_items mi
+        JOIN
+          order_items oi ON mi.id = oi.menu_item_id
+        JOIN
+          orders o ON oi.order_id = o.id
+        WHERE
+          o.timestamp >= ${startDate.toISOString()} AND
+          o.timestamp <= ${endDate.toISOString()}
+        GROUP BY
+          mi.id;
+      `;
+      
+      const items: AggregateItem[] = [];
+      const result = await isql.any(query);
+      
+      for (const row of result) {
+        items.push(new AggregateItem(row.id, row.name, row.qty));
+      }
+      
+      return items;
 
     });
 }
