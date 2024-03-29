236c8ca3e9ae524cfdb0cb807a1ed7f3c76b88ba
Sandeep <sandeep2002mishra@tamu.edu>
2024-03-28 20:26:34 -0500

    Added the proper parameters and definition to the aggregateMenuItems function

--- src/lib/menuBoard.ts
+++ src/lib/menuBoard.ts
@@ -1,4 +1,12 @@
 import psql, { transact } from "@/lib/database"
-import { Order } from "@/lib/models";
+import { MenuItem } from "@/lib/models";
+import { Seasonal } from "@/lib/models";
 import Error from "@/lib/error";
 import postgres from "postgres";
+
+export async function aggregateMenuItems(startDate: Date, endDate: Date, tsql = psql): Promise<AggregateItem[]>{
+    return transact<AggregateItem[], postgres.Error, any>(tsql, new Error("SQL Error in removeFromInventory", undefined, {startDate, endDate}), async (isql, abort) => {
+        
+
+    });
+}
