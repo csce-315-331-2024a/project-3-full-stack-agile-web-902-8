0aabc92d64590fe05771ee2324fdbf80546afcfb
Marvin Fung <marfung37@tamu.edu>
2024-03-31 13:46:35 -0500

    Working

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 import styles from "@/app/page.module.css";
 import customerStyles from "@/app/customer/page.module.css";
+
+import DoubleText from "@/components/DoubleText";
+import SideBar from "@/components/SideBar";
 import CustomerMenuItem from "@/components/CustomerMenuItem";
 import CustomerNav from "@/app/customer/customer-nav";
 import React from "react";
@@ -24,40 +27,58 @@ export default function Customer() {
     let setPopUp: Function;
     [showPopUp, setPopUp] = React.useState(false);
 
+    const Items2 = ['Manager', 'Customer', 'Cashier', 'MenuBoard'];
+    const Links2 = ['/manager', '/customer', '/cashier', '/menuboards'];
+
     return (
-      // TODO: Change to global styling
-      <main className={customerStyles.main}>
-        <h1 className={customerStyles.pageTitle}>Customer</h1>
+        // TODO: Change to global styling
+        <main className={styles.main}>
+          <div className = {styles.description}>
+            <div>
+                <CustomerNav/>
+            </div>
+            <div className={styles.body}>
+              <DoubleText 
+                block1 = <SideBar names={Items2} hrefs={Links2}/>
+                block2 = 
+                <div className={customerStyles["main"]}>
+                    <h1 className={customerStyles.pageTitle}>Customer</h1>
+                    
+                    <nav id={customerStyles["categories"]}>
+                      <ul>
+                        {categoryNames.map((name) => 
+                          <li key={name}><button onClick={() => changeCategory(name)}>{name}</button></li>
+                        )}
+                      </ul>
+                    </nav>
+                    
+                    {/* Menu items */}
+                    <div id={customerStyles["menu-items"]}>
+                      {menuItemsByCategory.get(currCategory)!.map((menuitem: string) =>
+                        <CustomerMenuItem key={menuitem} name={menuitem} onClick={() => setPopUp(true)}/>
+                      )}
+                    </div>
 
-        <CustomerNav/>
-        
-        <nav id={customerStyles["categories"]}>
-          <ul>
-            {categoryNames.map((name) => 
-              <li key={name}><button onClick={() => changeCategory(name)}>{name}</button></li>
-            )}
-          </ul>
-        </nav>
-        
-        {/* Menu items */}
-        <div id={customerStyles["menu-items"]}>
-          {menuItemsByCategory.get(currCategory)!.map((menuitem: string) =>
-            <CustomerMenuItem key={menuitem} name={menuitem} onClick={() => setPopUp(true)}/>
-          )}
-        </div>
+                    {/* Pop up for adding to order */}
+                    { showPopUp ? 
+                        <section id={customerStyles["pop-up"]}>
+                          {/* Exit button */}
+                          <button 
+                            className={customerStyles["exit-button"]} 
+                            onClick={() => setPopUp(false)}
+                            >
+                            X
+                          </button>
+                        </section>
+                    : null }
 
-        {/* Pop up for adding to order */}
-        { showPopUp ? 
-            <section id={customerStyles["pop-up"]}>
-              {/* Exit button */}
-              <button 
-                className={customerStyles["exit-button"]} 
-                onClick={() => setPopUp(false)}
-                >
-                X
-              </button>
-            </section>
-        : null }
-      </main>
+
+                </div>
+            />
+            </div>
+          </div>
+        </main>
+
+
     );
 }
