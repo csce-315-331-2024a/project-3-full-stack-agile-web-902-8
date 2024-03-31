ff2fdea1e4ca1a099c5aac7ba44f635760599b18
Kyle Palermo <kylepalermo@tamu.edu>
2024-03-31 15:46:44 -0500

    Added temporary logout link to /

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,18 +1,21 @@
 'use client';
 import React from 'react';
+import Link from 'next/link';
 import componentStyles from './component.module.css';
 
 function LogoutButton(){
     const handleLogout = () => {
-        // Call the logout function
+        // TODO: implement logout
         console.log("Logout button clicked");
     };
 
     return (
         <div className={componentStyles.logoutDiv}>
-            <button className={componentStyles.logout + ' ' + componentStyles.card} onClick={handleLogout}>
-                Logout
-            </button>
+            <Link href="/">
+                <button className={componentStyles.logout + ' ' + componentStyles.card} onClick={handleLogout}>
+                    Logout
+                </button>
+            </Link>
         </div>
     );
 };
