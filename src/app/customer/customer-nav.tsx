c4a4f94188efd5119432e11a8dde79c29c3b1218
Marvin Fung <marfung37@tamu.edu>
2024-03-31 15:40:07 -0500

    Added logout to customer page

--- src/app/customer/customer-nav.tsx
+++ src/app/customer/customer-nav.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Heading from "@/components/Heading";
 
 function CustomerNav() {
-    const pages = ["home", "order", "checkout"];
-    const hrefs = ["/customer", "/customer/order", "/customer/checkout"];
+    const pages = ["home", "order", "checkout", "logout"];
+    const hrefs = ["/customer", "/customer/order", "/customer/checkout", "/"];
 
     return (
         <Heading names={pages} hrefs={hrefs}/>
