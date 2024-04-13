5a6dc75fb3f10369df7b29cc656ab790e603b7b1
Jose-Ortiz47 <josepascualos17@tamu.edu>
2024-04-13 12:03:26 -0500

    Implemented format of tables

--- src/app/manager/report_page/page.tsx
+++ src/app/manager/report_page/page.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import Heading from '@/components/Heading';
 import styles from '@/app/page.module.css';
+import tables from '@/components/component.module.css'
 
 export default function MenuBoard() {
     const items = ['', 'Back'];
@@ -10,18 +12,15 @@ export default function MenuBoard() {
         <main className={styles.main}>
             <div className={styles.description}>
                 <Heading names={items} hrefs={links} />
-                <header>Hello I am Jose. I love Zach</header>
 
                 {/* Table Component */}
-                <table className={styles.table}>
+                <div>
+                <table className={tables.table}>
                     <thead>
                         <tr>
-                            <th>ID</th>
                             <th>Name</th>
-                            <th>Price</th>
-                            <th>Price</th>
-                            <th>Price</th>
-
+                            <th>10% Quantity</th>
+                            <th>Maximum Quantity</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,6 +38,34 @@ export default function MenuBoard() {
                         {/* Add more rows as needed */}
                     </tbody>
                 </table>
+                </div>
+
+                <div>
+                <table className={tables.table}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Quantity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        {/* figure out a way to go through specified range to display */}
+                            <td>1</td>
+                            <td>Item 1</td>
+                            <td>$10</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Item 2</td>
+                            <td>$15</td>
+                        </tr>
+                        {/* Add more rows as needed */}
+                    </tbody>
+                </table>
+                </div>
+                
             </div>
         </main>
     );
