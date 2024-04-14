d185b3dd73e94f5b09a4ad47dddaa9290bb4b08f
tran-nathan36 <tran.nathan36@tamu.edu>
2024-04-13 19:03:32 -0500

    Fixed prettier

--- src/app/manager/inventory/page.tsx
+++ src/app/manager/inventory/page.tsx
@@ -42,7 +42,7 @@ export default function Inventory() {
                             <h1>Manage Inventory</h1>
 
                             <PageButton>Refresh</PageButton>
-                            <InventoryAdjuster/>
+                            <InventoryAdjuster />
                         </div>
                     />
                 </div>
