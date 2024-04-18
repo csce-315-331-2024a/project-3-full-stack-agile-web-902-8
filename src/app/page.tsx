643332e550c7f4e20e69d52d9fb53d1470f19111
Jose-Ortiz47 <josepascualos17@tamu.edu>
2024-04-18 15:36:39 -0500

    root page customer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,12 @@
 'use client';
-import React from 'react';
-import Heading from '@/components/Heading';
-import DoubleText from '@/components/DoubleText';
-import SideBar from '@/components/SideBar';
-import styles from '@/app/page.module.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function Manager() {
-    const openMenuBoardsPages = () => {
-        window.open('/menuboards/Burgs', '_blank');
-        window.open('/menuboards/Meals_Limited', '_blank');
-        window.open('/menuboards/Misc', '_blank');
-        window.open('/menuboards/Sandwiches_Baskets', '_blank');
-    };
+export default function Home(){
+  const { push } = useRouter();
 
-    const Items = ['Manager', 'Customer', 'Cashier', 'MenuBoard'];
-    const Links = ['/manager', '/customer', '/cashier', '/'];
-
-    return (
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <div className={styles.body}>
-                    <DoubleText
-                        block1={
-                            <SideBar
-                                names={Items}
-                                hrefs={Links}
-                                onClick={openMenuBoardsPages}
-                            />
-                        }
-                        block2={undefined}
-                    />
-                </div>
-            </div>
-        </main>
-    );
-}
+  useEffect(() => {
+     push('/customer');
+  }, []);
+  return (<p></p>);
+};
