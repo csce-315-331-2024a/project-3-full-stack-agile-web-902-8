04a77bf958a4586d99d429dfa741e7786aa75db4
Kyle Palermo <kylepalermo@tamu.edu>
2024-04-21 04:44:58 -0500

    Updated navbar style

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from './component.module.css';
 
 type HeadingProp = {
@@ -8,11 +11,18 @@ type HeadingProp = {
 };
 
 function Heading({ names, hrefs }: HeadingProp) {
+    const pathName = usePathname();
+
     return (
         <nav className={styles.navbar}>
             <ul>
                 {names.map((link, i) => (
-                    <li key={link}>
+                    <li
+                        key={link}
+                        className={
+                            pathName === hrefs[i] ? styles.navbarCurrent : ''
+                        }
+                    >
                         <Link href={hrefs[i]}>{link}</Link>
                     </li>
                 ))}
