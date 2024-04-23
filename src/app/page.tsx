fb68c586000a926e0ea09b5408e48f6b716ce576
Marvin Fung <marfung37@tamu.edu>
2024-04-23 09:17:32 -0500

    Got OAuth to appear to be working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-export default function Home() {
-    const { push } = useRouter();
 
-    useEffect(() => {
-        push('/customer');
-    }, [push]);
-    return <main>Redirecting...</main>;
+import { getUserSession } from '@/lib/session'
+
+export default async function Home() {
+  const user = await getUserSession()
+  return <main className="">{JSON.stringify(user)}</main>
 }
+
+// export default function Home() {
+//     const { push } = useRouter();
+//
+//     useEffect(() => {
+//         push('/customer');
+//     }, [push]);
+//     return <main>Redirecting...</main>;
+// }
