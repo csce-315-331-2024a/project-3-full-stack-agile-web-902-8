e81454a31507df3c7d8e0844ac4b417be4d03a09
blubywaff <45488563+blubywaff@users.noreply.github.com>
2024-04-27 17:12:44 -0500

    make recommendedItems dynamic

--- src/app/api/recommendedItems/route.ts
+++ src/app/api/recommendedItems/route.ts
@@ -2,6 +2,8 @@ import { getWeatherRecommendations } from "@/lib/menu";
 import { getWeatherData } from "@/lib/weather";
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
     console.log("GET /api/recommendedItems");
     try {
