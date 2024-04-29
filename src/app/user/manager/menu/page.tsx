825a714f84685500bc71f1c6da0264e8114d18a4
Sandeep <sandeep2002mishra@tamu.edu>
2024-04-28 23:30:12 -0500

    Manager pages all have zoom and it should be working

--- src/app/user/manager/menu/page.tsx
+++ src/app/user/manager/menu/page.tsx
@@ -6,17 +6,14 @@ export default function Manager() {
     return (
         <ScaleProvider initialScale={1}>
         {/* Scaled content */}
-        
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <div className={styles.body}>
-                    <div>
-                        <h1>Manager Page</h1>
-                        <PageButton>Refresh</PageButton>
-                        <MenuEditer />
-                    </div>
-                </div>
-            </div>
+        <main className="col-[2/3] row-[2/3] overflow-y-auto overflow-x-hidden w-full h-full grid grid-cols-[auto_auto] grid-rows-[auto_1fr] gap-4 p-4">
+            <h1 className="col-[1/2] row-[1/2] text-[4rem] font-bold relative mainHeader w-fit">
+                Manage Menu
+            </h1>
+            <button className="col-[2/3] row-[1/2] bg-secondary duration-200 hover:bg-secondary/50 rounded-2xl p-4 m-[0_0_auto_auto]">
+                Refresh
+            </button>
+            <MenuEditer className="col-[1/3] row-[2/3] mx-auto" />
         </main>
     {/* Fixed-position zoom controls */}
     <div
