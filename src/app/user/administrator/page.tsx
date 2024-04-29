67e6c41dc3fdf64a8c340b0b1f5d253023236a61
Sandeep <sandeep2002mishra@tamu.edu>
2024-04-29 02:27:46 -0500

    zoom buttons should be viewable and should work for every screen

--- src/app/user/administrator/page.tsx
+++ src/app/user/administrator/page.tsx
@@ -38,7 +38,7 @@ export default function Administrator() {
                     top: '40px',
                     left: '50%',
                     transform: 'translateX(-50%)',
-                    zIndex: 1001, // Above scaled content
+                    zIndex: 1001, 
                     textAlign: 'center',
                 }}
             >
