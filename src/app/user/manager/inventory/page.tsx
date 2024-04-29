ad9b194f4a7131f7506cba56fb5fe05cf029e5e3
Kyle Palermo <kylepalermo@tamu.edu>
2024-04-28 21:00:38 -0500

    Added styling to menu manager page

--- src/app/user/manager/inventory/page.tsx
+++ src/app/user/manager/inventory/page.tsx
@@ -1,14 +1,7 @@
-// DONE
-
-// TODO: responsiveness
 'use client';
-import React, { useEffect, useState } from 'react';
 import InventoryAdjuster from '@/components/InventoryAdjuster';
-import { InventoryItem } from '@/lib/models';
 
 export default function Inventory() {
-    const inventoryItem: InventoryItem = new InventoryItem(0, '', 0, 0, 0, 0);
-
     return (
         <main className="col-[2/3] row-[2/3] overflow-y-auto overflow-x-hidden w-full h-full grid grid-cols-[auto_auto] grid-rows-[auto_1fr] gap-4 p-4">
             <h1 className="col-[1/2] row-[1/2] text-[4rem] font-bold relative mainHeader w-fit">
