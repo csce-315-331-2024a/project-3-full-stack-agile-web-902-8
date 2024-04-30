294351bf1056e7c23bcb2226b0a4508d94d5a0a2
Marvin Fung <marfung37@tamu.edu>
2024-04-29 22:57:04 -0500

    Added styling to globals.css to remove google translate topbar

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -66,7 +66,7 @@ function Heading({
                     )
                 )}
             </ul>
-            <GoogleTranslate></GoogleTranslate>
+            <GoogleTranslate/>
             <LoginButton isLoggedIn={isLoggedIn} />
         </nav>
     );
