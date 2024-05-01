8974200d4e0a1c6a221ecc9a11a3ed2124cc2f25
blubywaff <45488563+blubywaff@users.noreply.github.com>
2024-05-01 13:27:06 -0500

    add real tests for customer and remove premade examples

---
+++ tests/customer.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('root redirect', async ({ page }) => {
+    await page.goto('/');
+    // Should not take a noticable amount of time to redirect.
+    await page.waitForURL('**/user/customer', { timeout: 200 })
+});
+
+test('images load', async ({ page }) => {
+    // 'networkidle' is strongly discouraged, but we need it to know that all images have loaded
+    await page.goto('/user/customer', { waitUntil: 'networkidle' });
+    let imgs = await page.locator('//img').all();
+    let imgsLoaded: boolean[] = await Promise.all(imgs.map(async img => { return (await img.getAttribute('height')) !== '0'; }));
+    expect(imgsLoaded.every(x => x === true)).toBeTruthy();
+});
+
+test('correct title', async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle("Rev's American Grill");
+});
